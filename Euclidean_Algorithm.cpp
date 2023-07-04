56ec3682ad1fa6c22cfa8f1903a3eff92a024900
Mihail <mihail.larionow2001@gmail.com>
2023-07-04 16:44:20 +0300

    Added the Euclidean Algorithm

---
+++ Euclidean_Algorithm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int EuclideanAlgorithm(int a, int b){
+
+    if(a == 0) return b;
+    return EuclideanAlgorithm(b % a, a);
+}
+
+
+
+int main()
+{
+    std::cout<<EuclideanAlgorithm(15, 21);
+}
