4f305e22ae6d56d8a92b471fffd84effc3afbb67
Mihail <mihail.larionow2001@gmail.com>
2023-07-06 00:07:35 +0300

    Update Euclidean_Algorithm.cpp

--- Euclidean_Algorithm.cpp
+++ Euclidean_Algorithm.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
-int EuclideanAlgorithm(int a, int b)
+int euclideanAlgorithm(int a, int b)
 {
     if (a == 0)
         return b;
-    return EuclideanAlgorithm(b % a, a);
+    return euclideanAlgorithm(b % a, a);
+}
+
+void commonFactors(int a, int b) {
+    int nod = euclideanAlgorithm(a, b);
+    for(int i=1; i<=nod; i++){
+        if(nod % i == 0) std::cout << i << " ";
+    }
 }
 
 int main()
 {
-    std::cout << EuclideanAlgorithm(15, 21);
+    std::cout << euclideanAlgorithm(15, 21) << "\n";
+    commonFactors(15, 21);
 }
