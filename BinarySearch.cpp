08a5e997a8ea1dd0c6d5c8ecf98a23260029b8de
Mihail <mihail.larionow2001@gmail.com>
2023-07-11 19:52:21 +0300

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
-int binarySearch(int arr[], int size_t, int value){
+int binarySearch(int arr[], int size_t, int value)
+{
     int left = 0;
     int right = size_t - 1;
-    while(left <= right){
+    while (left <= right)
+    {
         int middle = (left + right) / 2;
-        if(value == arr[middle]) return middle;
-        else if(value > arr[middle]) left = middle + 1;
-        else right = middle - 1;
+        if (value == arr[middle])
+            return middle;
+        else if (value > arr[middle])
+            left = middle + 1;
+        else
+            right = middle - 1;
     }
     return -1;
 }
@@ -17,5 +22,5 @@ int main()
     int arr[5]{3, 2, 1, 5, 4};
     int size_t = sizeof(arr) / sizeof(arr[0]);
 
-    std::cout << binarySearch(arr, size_t, 5) << "\n";
+    std::cout << binarySearch(arr, size_t, 5); // O(log(2n))
 }
