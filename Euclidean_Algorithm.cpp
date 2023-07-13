df31a322ce8b0735fdeb662035269ab33474613d
Mihail <mihail.larionow2001@gmail.com>
2023-07-13 21:12:06 +0300

    Added the LCM algorithm

--- Euclidean_Algorithm.cpp
+++ Euclidean_Algorithm.cpp
@@ -7,15 +7,24 @@ int euclideanAlgorithm(int a, int b)
     return euclideanAlgorithm(b % a, a);
 }
 
-void commonFactors(int a, int b) {
+int leastCommonMultiple(int a, int b)
+{
+    return (a * b) / euclideanAlgorithm(a, b);
+}
+
+void commonFactors(int a, int b)
+{
     int nod = euclideanAlgorithm(a, b);
-    for(int i=1; i<=nod; i++){
-        if(nod % i == 0) std::cout << i << " ";
+    for (int i = 1; i <= nod; i++)
+    {
+        if (nod % i == 0)
+            std::cout << i << " ";
     }
 }
 
 int main()
 {
     std::cout << euclideanAlgorithm(15, 21) << "\n";
+    std::cout << leastCommonMultiple(15, 21) << "\n";
     commonFactors(15, 21);
 }
