d1004089c8bc5b8fbb308102a693704e2121c593
Mihail <mihail.larionow2001@gmail.com>
2023-07-29 16:04:55 +0300

    Added the Hash Table structure

---
+++ data structures/HashTable.cpp
@@ -0,0 +1,17 @@
+class HashTable
+{
+    public:
+    private:
+    int getHash(int number){
+        int hash = number;
+        return hash;
+    }
+};
+
+int main(){
+    int arr[10]{5, 6, 7, 3, 2, 1, 9, 8, 10, 4};
+    int size_t = sizeof(arr) / sizeof(arr[0]);
+
+    HashTable *table = new HashTable;
+}
+
