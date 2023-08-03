c1751034497088f2750ab60dfec6a579441be405
Mihail <mihail.larionow2001@gmail.com>
2023-08-03 21:48:57 +0300

    Update HashTable.cpp

--- data structures/HashTable.cpp
+++ data structures/HashTable.cpp
@@ -1,17 +1,50 @@
+#include <iostream>
+
 class HashTable
 {
-    public:
-    private:
-    int getHash(int number){
-        int hash = number;
-        return hash;
+public:
+    void add(std::string key, std::string value)
+    {
+        int index = getHash(key);
+        items[index] = value;
+    }
+
+    std::string get(std::string key){
+        int index = getHash(key);
+        return items[index];
+    }
+
+    void destroy(){
+        delete [] items;
+    }
+
+private:
+    int size = 5000;
+    int count;
+
+    std::string *items = new std::string[size];
+
+    int getHash(std::string key)
+    {
+        int hash = 0;
+
+        for(int i=0; i<key.size(); i++){
+            hash += key[i];
+        }
+
+        return hash % size;
     }
 };
 
-int main(){
-    int arr[10]{5, 6, 7, 3, 2, 1, 9, 8, 10, 4};
-    int size_t = sizeof(arr) / sizeof(arr[0]);
-
+int main()
+{
     HashTable *table = new HashTable;
-}
 
+    table->add("firstname", "Mihail");
+    table->add("lastname", "Larionov");
+
+    std::cout << "My FirstName is " << table->get("firstname") << "\n";
+    std::cout << "My LastName is " << table->get("lastname") << "\n";
+
+    table->destroy();
+}
