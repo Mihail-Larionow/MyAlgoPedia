5648153fac7a6fbf4d26e4832a9a4a6aa2c3562c
Mihail <mihail.larionow2001@gmail.com>
2023-09-05 22:03:39 +0300

    Create Graph.cpp

---
+++ data structures/Graph.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+const int INF = 1e9;
+
+std::vector<int> bfs(std::vector<std::vector<int>> &graph, int start)
+{
+    std::vector<int> dist(graph.size(), INF);
+    std::queue<int> q;
+    dist[start] = 0;
+    q.push(start);
+
+    while (!q.empty())
+    {
+        int v = q.front();
+        q.pop();
+
+        for (int to : graph[v])
+        {
+            if (dist[to] > dist[v] + 1)
+            {
+                dist[to] = dist[v] + 1;
+                q.push(to);
+            }
+        }
+    }
+    return dist;
+}
+
+int main()
+{
+    int n, m, x;
+
+    std::cin >> n >> m >> x;
+
+    std::vector<std::vector<int>> graph(n);
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    std::vector<int> dist = bfs(graph, x);
+    for (int d : dist)
+    {
+        if (d != INF)
+            std::cout << d << " ";
+        else
+            std::cout << "ERR";
+    }
+}
