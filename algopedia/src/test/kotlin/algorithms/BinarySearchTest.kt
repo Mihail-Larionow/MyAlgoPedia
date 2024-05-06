28c8ae244b3b322c74f2fb351bb9ae88f2876dd6
Mihail <mihail.larionow2001@gmail.com>
2024-05-06 21:45:52 +0300

    IDE Updates

--- algopedia/src/test/kotlin/algorithms/BinarySearchTest.kt
+++ algopedia/src/test/kotlin/algorithms/BinarySearchTest.kt
@@ -1,33 +1,50 @@
 package algorithms
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-
 
 class BinarySearchTest {
 
     private val array = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
     private val bs = BinarySearch()
 
     @Test
-    fun `should return value`(){
+    fun `should return key first`(){
         val x = 6
 
         val expected = 5
         val actual = bs.execute(arr = array, value = x)
 
-        assertEquals(expected, actual)
+        assertEquals(expected = expected, actual = actual)
     }
 
     @Test
-    fun `shouldn't return value`(){
+    fun `should return key second`(){
+        val x = 6
+
+        val expected = 5
+        val actual = bs.execute(arr = array, value = x)
+
+        assertEquals(expected = expected, actual = actual)
+    }
+
+    @Test
+    fun `should return key third`(){
+        val x = 6
+
+        val expected = 5
+        val actual = bs.execute(arr = array, value = x)
+
+        assertEquals(expected = expected, actual = actual)
+    }
+
+    @Test
+    fun `shouldn't return key`(){
         val x = 11
 
         val expected = -1
         val actual = bs.execute(arr = array, value = x)
 
-        Assertions.assertEquals(expected, actual)
+        assertEquals(expected = expected, actual = actual)
     }
 }
